7e0871ac67ebbb9fd270b7c405e5b1e7bca9ec85
Laura Hoang <laurahoangpl@gmail.com>
2025-04-07 20:59:33 +0200

    add app1.py and other files

---
+++ app1.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Witaj w moim API!!!"
+    # return jsonify({"message": "Witaj w moim API!"})
+
+@app.route('/api/v1.0/predict', methods=['GET'])
+def predict():
+    num1 = request.args.get("num1", "")
+    num2 = request.args.get("num2", "")
+
+    if not num1:
+        num1 = 0
+    if not num2:
+        num2 = 0
+
+    num1 = float(num1)
+    num2 = float(num2)
+
+    resp = (num1+num2 > 5.8)*1
+
+    return jsonify({"prediction": resp}, {"features": {"num1": num1, "num2": num2}})
+
+if __name__ == '__main__':
+    app.run(port=5005)
